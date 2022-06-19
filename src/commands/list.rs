b08be2ea213bd0ba37532cff149d8adf517ae430
Dietrich <dietrich@teilgedanken.de>
2022-06-19 19:05:33 +0200

    prove of concept api call

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -1,31 +1,85 @@
-use std::io::Read;
-
+use serde::Deserialize;
 use serenity::framework::standard::macros::command;
 use serenity::framework::standard::CommandResult;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
+#[derive(Deserialize, Debug)]
+struct Community {
+    name: String,
+    city: String,
+    #[serde(alias = "upcomingEvents")]
+    upcomming_events: UpcomingEvents,
+}
 
-use web_ical::Calendar;
+#[derive(Deserialize, Debug)]
+struct UpcomingEvents {
+    count: usize,
+    #[serde(alias = "edges")]
+    meetings: Vec<Node>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Node {
+    node: Meeting,
+}
+
+#[derive(Deserialize, Debug)]
+struct Data {
+    #[serde(alias = "groupByUrlname")]
+    community: Community,
+}
+
+#[derive(Deserialize, Debug)]
+struct Meeting {
+    title: String,
+    #[serde(alias = "eventUrl")]
+    event_url: String,
+    status: String,
+    #[serde(alias = "dateTime")]
+    date_time: String,
+    id: String,
+}
 
 #[command]
 pub async fn list(ctx: &Context, msg: &Message) -> CommandResult {
-    println!("Listing some ical events");
+    let query = r#"query  {
+        groupByUrlname(urlname: "rust-community-stuttgart") {
+          name
+          city   
+          upcomingEvents(input: {first:3, last:10}){
+            count
+            edges{
+              node{
+                title
+                eventUrl
+                status
+                dateTime
+                id
+              }
+            }
+            
+          }
+        }
+      }
+    "#;
+    let endpoint = "https://api.meetup.com/gql";
+    let client = gql_client::Client::new(endpoint);
 
-    let mut file =
-        std::fs::File::open("/home/dietrich/Downloads/ics").expect("Failed to open file");
-    let mut data = String::new();
-    file.read_to_string(&mut data).expect("Failed to read Data");
+    let response = client.query::<Data>(query).await.unwrap().unwrap();
 
-    println!("{}", data);
+    let events: Vec<Meeting> = response
+        .community
+        .upcomming_events
+        .meetings
+        .into_iter()
+        .map(|x| x.node)
+        .collect();
 
-    let events = Calendar::new_from_data(&data).expect("Failed to parse events");
-
-    println!("{:?}", events.events.len());
-    println!("{:?}", events.version);
-
-    for line in events.events {
-        println!("Item {:#?}", &line.summary);
-        msg.channel_id.say(&ctx.http, "test").await?;
+    for line in events {
+        println!("Item {:#?}", &line);
+        msg.channel_id
+            .say(&ctx.http, format!("Item {:#?}", line))
+            .await?;
     }
 
     Ok(())
